d32b3e41e53f957c11e9b5bd2ba15530feda075d
Atifsid <atifsiddiqui35@gmail.com>
2020-01-09 15:19:28 +0530

    santoni: Change path for cosmic

--- camera/QCamera2/Android.mk
+++ camera/QCamera2/Android.mk
@@ -86,8 +86,8 @@ LOCAL_C_INCLUDES := \
         $(LOCAL_PATH)/stack/mm-camera-interface/inc \
         $(LOCAL_PATH)/util \
         $(LOCAL_PATH)/HAL3 \
-        hardware/qcom-caf/msm8996/media/libstagefrighthw \
-        hardware/qcom-caf/msm8996/media/mm-core/inc \
+        hardware/qcom/media/msm8996/libstagefrighthw \
+        hardware/qcom/media/msm8996/mm-core/inc \
         $(TARGET_OUT_HEADERS)/mm-camera-lib/cp/prebuilt
 
 LOCAL_HEADER_LIBRARIES := media_plugin_headers
@@ -122,7 +122,7 @@ LOCAL_STATIC_LIBRARIES := android.hardware.camera.common@1.0-helper
 LOCAL_C_INCLUDES += \
         $(TARGET_OUT_HEADERS)/qcom/display
 LOCAL_C_INCLUDES += \
-        hardware/qcom-caf/msm8996/display/libqservice
+        hardware/qcom/display/msm8996/libqservice
 LOCAL_SHARED_LIBRARIES := liblog libhardware libutils libcutils libdl libsync
 LOCAL_SHARED_LIBRARIES += libmmcamera_interface libmmjpeg_interface libui libcamera_metadata
 LOCAL_SHARED_LIBRARIES += libqdMetaData libqservice libbinder
